f2ea07ebc64b6eb2b7b1482d86098f1c2d782f64
Alaa Bawaneh <bawanehcoder@live.com>
2022-08-28 14:34:31 +0500

    some fix in offers

--- app/Http/Resources/OfferResource.php
+++ app/Http/Resources/OfferResource.php
@@ -14,6 +14,9 @@ class OfferResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'=>$this->id,
+            'image'=>$this->image
+        ];
     }
 }
