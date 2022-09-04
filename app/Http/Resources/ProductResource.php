1119c292fca7fe3d588b3bc4730feaa7cfc623fb
Alaa Bawaneh <bawanehcoder@live.com>
2022-09-04 12:47:26 +0500

    change resource for product

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,9 +14,12 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
+        $c =array('1'=>'name');
         return [
             'id'=>$this->id,
             'name'=>$this->name,
+            'price'=>$this->price,
+            'categorise'=>$c,
             'sizes'=>$this->sizes,
             'extras'=>$this->extras,
         ];
