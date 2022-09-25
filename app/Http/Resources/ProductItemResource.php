dc2ce49bb6ab43d3e8e6175a46cf8feadddf141b
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-09-25 15:54:48 +0300

    story fix

--- app/Http/Resources/ProductItemResource.php
+++ app/Http/Resources/ProductItemResource.php
@@ -15,7 +15,7 @@ class ProductItemResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
+            'id'=>$this->extra->id,
             'name'=>$this->extra->name,
             'price'=>$this->extra->price,
         ];
