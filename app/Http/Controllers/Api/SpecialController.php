e87f9dc6bbdcdd1e1ad152a56877e377520fdb59
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2022-11-25 13:30:42 +0200

    offerable improve

--- app/Http/Controllers/Api/SpecialController.php
+++ app/Http/Controllers/Api/SpecialController.php
@@ -51,12 +51,18 @@ class SpecialController extends Controller
     public function store(SpecialRequest $request)
     {
         if (isset($request->resturant_id)) {
-            $request['offerable_id']=$request->resturant_id;
             $resturant = Restaurant::find($request->resturant_id);
+            if (!$resturant) {
+                return $this->returnError('This resturant is not exists');
+            }
+            $request['offerable_id']=$request->resturant_id;
             $request['offerable_type']=get_class($resturant);
         }elseif (isset($request->product_id)) {
-            $request['offerable_id']=$request->product_id;
             $product = Product::find($request->product_id);
+            if (!$product) {
+                return $this->returnError('This product is not exists');
+            }
+            $request['offerable_id']=$request->product_id;
             $request['offerable_type']=get_class($product);
         }elseif(isset($request->resturant_id) && isset($request->product_id)){
             return $this->returnError('It\'s not allowed to add both (resturant_id and product_id) Please add only one of them');
