71f5816baec08bb5d457d059c37fdcad2074bfac
Alaa Bawaneh <bawanehcoder@live.com>
2022-11-26 14:45:23 +0300

    fix checkout

--- app/Http/Resources/CartItemResource.php
+++ app/Http/Resources/CartItemResource.php
@@ -16,7 +16,7 @@ class CartItemResource extends JsonResource
     {
         return [
             'id'=>$this->id,
-            'group_name'=>$this->group->name,
+            //'group_name'=>$this->group->name,
             'item_name'=>$this->item->name,
             'price'=>number_format($this->item->price,2),
         ];
