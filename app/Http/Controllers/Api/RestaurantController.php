fbffada19f91f562efb2b8f31d11df0982f0f4be
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-11-27 03:09:35 +0300

    last check

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -51,8 +51,23 @@ class RestaurantController extends ApiController
 
     public function getPagination( Request $request )
     {
-
-            $data =  $this->repositry->pagination(10);
+        if (!isset($request->category_id) && !isset($request->tag_id)) {
+            $data =  $this->repositry->with('review')->pagination(10);
+        }elseif (isset($request->category_id) && isset($request->tag_id)) {
+            $data =  $this->model->with('review')->whereHas('categories',function(Builder $q) use ($request){
+                $q->where('category_id',$request->category_id);
+            })->whereHas('tags',function(Builder $q) use ($request){
+                $q->where('tag_id',$request->tag_id);
+            })->paginate( 10 );
+        }elseif (isset($request->category_id) && !isset($request->tag_id)) {
+            $data =  $this->model->with('review')->whereHas('categories',function(Builder $q) use ($request){
+                $q->where('category_id',$request->category_id);
+            })->paginate( 10 );
+        }elseif (isset($request->tag_id) && !isset($request->category_id)) {
+            $data =  $this->model->with('review')->whereHas('tags',function(Builder $q) use ($request){
+                $q->where('tag_id',$request->tag_id);
+            })->paginate( 10 );
+        }
 
         // return json_encode($all);
         return $this->returnData('data', ResturantRerviewResource::collection($data), '');    }
