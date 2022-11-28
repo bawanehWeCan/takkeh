aacf18e5cb8891da5ca73c3cf21bb7ef48ab0d59
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-11-28 12:28:20 +0300

    final otp

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -358,7 +358,7 @@ class AuthController extends Controller
     public function checkOTP( $phone, $otp ){
         $user = User::where('phone', $phone)->first();
 
-        if( $user->$otp == $otp ){
+        if( (string)$user->$otp == (string)$otp ){
             return true;
         }
 
