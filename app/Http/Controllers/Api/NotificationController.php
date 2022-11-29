e2869d9e945d567581b832b803b3f4845f2a215d
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-11-29 12:26:41 +0300

    fix

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -22,9 +22,44 @@ class NotificationController extends ApiController
         $this->repositry =  new Repository($this->model);
     }
 
-    public function save( NotificationRequest $request ){
+    public function save(NotificationRequest $request)
+    {
 
-        return $this->store( $request->all() );
+        return $this->store($request->all());
+    }
 
+
+    function send()
+    {
+        $token = "d1VC8jmZRL-4maUWM2che4:APA91bFQMZHwu38Tn-0kHMVAoCF0do1Uh2DB-dOSolvHU7977XWVmXCZ1zBiTyLekyHcCnXQMbrev9QYg6PBGQnLSDEd2xhOjpJomeEMn_nFJgZH0jv5Lh3LuIDm6W0PVe7FnNc6uSkR";
+        $from = "AAAA53N59_0:APA91bF27-YuWlYtfLoFYPDzsTqzL7VZvkTD5VsU3Gz9kh2thHQrn0HLm3lG4oedlPBNsqR61th11zgCkb_zHuJwm96oZzm0UBkHItKKgkcYF5YvawN32URY2lZTLMg8GcYMmccdDJC8";
+        $msg = array(
+            'body'  => "Testing Testing",
+            'title' => "Hi, From Wecan",
+            'receiver' => 'Alaa',
+        );
+
+        $fields = array(
+            'to'        => $token,
+            'notification'  => $msg
+
+        );
+
+
+        $headers = array(
+            'Authorization: key=' . $from,
+            'Content-Type: application/json'
+        );
+        //#Send Reponse To FireBase Server
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);
+        dd($result);
+        curl_close($ch);
     }
 }
