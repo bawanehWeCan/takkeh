3281fbbf15130b0ec0650706055bc4412a2723b0
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-12-11 12:49:10 +0300

    add lat long

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -82,6 +82,10 @@ class OrderController extends Controller
             array_push($fire, $fireItem);
         }
 
+        print_r($address->lat, $address->long);
+
+        return;
+
 
         $stuRef = app('firebase.firestore')->database()->collection('orders')->newDocument();
         $stuRef->set([
@@ -170,4 +174,22 @@ class OrderController extends Controller
             dd( $th );
         }
     }
+
+    function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1515;
+        $unit = strtoupper($unit);
+
+        if ($unit == "K") {
+          return ($miles * 1.609344);
+        } else if ($unit == "N") {
+            return ($miles * 0.8684);
+          } else {
+              return $miles;
+            }
+      }
 }
