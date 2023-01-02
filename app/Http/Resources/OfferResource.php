08f9fadd5f0be77ad20737112bd669c06a398331
Alaa Bawaneh <a.bawaneh@wecan.jo>
2023-01-02 20:13:23 +0300

    fix

--- app/Http/Resources/OfferResource.php
+++ app/Http/Resources/OfferResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
+use App\Models\Restaurant;
+
 class OfferResource extends JsonResource
 {
     /**
@@ -14,6 +16,35 @@ class OfferResource extends JsonResource
      */
     public function toArray($request)
     {
+
+
+        if( $this->offerable_type == 'App\Models\Restaurant'){
+
+            $r = Restaurant::find($this->offerable_id);
+            return [
+                'id'=>$this->id,
+                'image'=>$this->image,
+                'offerable_id'=>$this->offerable_id,
+                'offerable_type'=>$this->offerable_type,
+
+                'restaurant_id'=>$r->id,
+                'title'=>$r->name,
+
+                'logo'=>$r->logo,
+            'cover'=>$thris->cover,
+            'review_icon'=>'img/cats/burger.svg',
+            'cost'=>'توصيل مجاني',
+            'time'=>$r->time,
+            'description'=>$r->description,
+            'is_busy'=>$r->is_busy,
+            'review_average'=>$r->review->avg('points'),
+            'review'=>$r->review_title,
+            'review_icon'=>$r->review_icon,
+
+                'route'=> 'restaurant',
+
+            ];
+        }
         return [
             'id'=>$this->id,
             'image'=>$this->image,
