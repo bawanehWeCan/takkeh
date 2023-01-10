021471f3340ac1139826d2690c47b8bb4e23678e
Alaa Bawaneh <a.bawaneh@wecan.jo>
2023-01-10 21:29:55 +0300

    fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -40,8 +40,8 @@ class Product extends Model
         return $this->morphToMany( Category::class, 'categoryable' );
     }
 
-    public function r(){
-        return $this->belongsTo(Restaurant::class);
+    public function restaurant(){
+        return $this->belongsTo(Restaurant::class, 'restaurant_id');
     }
 
 }
